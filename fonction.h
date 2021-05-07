362632db848fd586c37006bcd40ee86079d48be7
Orhane Lahneche <orhane.lahneche@etu.utc.fr>
2021-05-07 11:07:19 +0200

    "création structures"

--- fonction.h
+++ fonction.h
@@ -5,4 +5,16 @@
 #ifndef TP4_FONCTION_H
 #define TP4_FONCTION_H
 
+typedef struct ListeVaccins {
+    char* marque;
+    int nombre_de_vaccins;
+    struct ListeVaccins * suivant;
+}T_ListeVaccins;
+
+typedef struct ABR {
+    char* date;
+    T_ListeVaccins * listevaccins;
+    ABR* fils_gauche;
+    ABR* fils_droite;
+}T_ABR;
 #endif //TP4_FONCTION_H
